7eeece1b2900adab8d13af85776dfb6c5c6bd351
pilleliis <pilleliis@hotmail.com>
2024-05-09 15:18:28 +0300

    changed

---
+++ script.py
@@ -0,0 +1,9 @@
+import sys
+import argparse
+print("Hello, world!")
+
+parser = argparse.ArgumentParser(description='Procecc some integers.')
+parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer for the accumulator')
+
+args=
+print(sys.argv)
